51da97de060f7794553d959dffa17bea689977d2
max <max.couling@outlook.com>
2024-09-23 21:11:04 +1200

    Simple fix, added GET and POST method

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.forms import RegistrationForm, LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
