60953c60d0c2709e5fa8d502b6ace5a33f161886
max <max.couling@outlook.com>
2024-09-28 11:39:39 +1200

    Add Tailwind CSS configuration and compiled CSS

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */// tailwind.config.js
+module.exports = {
+  content: [
+    './app/templates/*.html', // Adjust the path to your templates
+    './app/static/**/*.js',      // If you have custom JS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ],
+  },
+}
+
